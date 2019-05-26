a7b1d779df2173a4262b652141cd9f68bde3499c
Salman Naseer <iit2015004@iiita.ac.in>
2019-05-26 15:11:44 +0530

    Add JS 3 API Calls Ez
    Twitter , Instagram and Youtube. 
    Figure out how to differentiate between ChannellD or Username

--- CodeAsylum-Hackathon/app.js
+++ CodeAsylum-Hackathon/app.js
@@ -1,21 +1,61 @@
 function Search() {
-    let username = document.getElementById('username').value;
-    console.log(username);
-    
-    let xhr = new XMLHttpRequest;
-    var resp = 0;
-    xhr.addEventListener('load',function(){
-        resp = JSON.parse(this.responseText)
-        console.log(resp.graphql.user.full_name);
-        document.getElementById('Enter').innerHTML += `<div> <p>${resp.graphql.user.full_name} : ${resp.graphql.user.edge_followed_by.count}</div>`;
-    })
 
-    xhr.addEventListener('error',function(){
-        console.log('error');
-    })
+  let fullname = document.getElementById('fullname').value;
+  let instahandle = document.getElementById('instahandle').value;
+  let twitterhandle = document.getElementById('twitterhandle').value;
+  let youtubehandle = document.getElementById('youtubehandle').value;
+  youtubehandle = (youtubehandle.split("/")[4])
+  console.log(youtubehandle)
+// xhr - instagram thr -twitter , yhr - youtube
+///Instagram
 
-    xhr.open('GET',`https://www.instagram.com/${username}/?__a=1`)
-    xhr.send()
+  let xhr = new XMLHttpRequest;
+  var resp = 0;
+  xhr.addEventListener('load',function(){
+      resp = JSON.parse(this.responseText)
+      console.log(resp.graphql.user.full_name);
+      document.getElementById('Enter').innerHTML += `<div> <p>${resp.graphql.user.full_name} : ${resp.graphql.user.edge_followed_by.count}</div>`;
+  })
 
-   
+  xhr.addEventListener('error',function(){
+      console.log('error');
+  })
+
+  xhr.open('GET',`https://www.instagram.com/${instahandle}/?__a=1`)
+  xhr.send()
+
+//// Twitter
+  let thr = new XMLHttpRequest;
+  var respt = 0;
+  thr.addEventListener('load',function(){
+      respt = JSON.parse(this.responseText)
+      console.log(respt[0].followers_count);
+      //document.getElementById('Enter').innerHTML += `<div> <p>${resp.graphql.user.full_name} : ${resp.graphql.user.edge_followed_by.count}</div>`;
+  })
+
+  thr.addEventListener('error',function(){
+      console.log('error');
+  })
+
+  thr.open('GET',`https://cors-anywhere.herokuapp.com/https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=${twitterhandle}`)
+  thr.send()
+
+///// Youtube
+
+
+  let yhr = new XMLHttpRequest;
+  var respy = 0;
+  yhr.addEventListener('load',function(){
+      respy = JSON.parse(this.responseText)
+      console.log(respy.items[0].statistics.videoCount)
+  })
+
+  yhr.addEventListener('error',function(){
+      console.log('error');
+  })
+
+  yhr.open('GET',`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubehandle}&key=AIzaSyB0UaawDVmtUHXPTIjkynBql-J1A6OlDbo`)
+  yhr.send()
+
+
 }
