b0b2873966bbb6b27844dc74b0c3fb3a91e81fa3
Neha Meena <nehameena7@gmail.com>
2019-05-26 15:04:11 +0530

    Partial CSS

---
+++ CodeAsylum-Hackathon/stylecss.js
@@ -0,0 +1,21 @@
+function Search() {
+    let username = document.getElementById('username').value;
+    console.log(username);
+    
+    let xhr = new XMLHttpRequest;
+    var resp = 0;
+    xhr.addEventListener('load',function(){
+        resp = JSON.parse(this.responseText)
+        console.log(resp.graphql.user.full_name);
+        document.getElementById('Enter').innerHTML += `<div> <p>${resp.graphql.user.full_name} : ${resp.graphql.user.edge_followed_by.count}</div>`;
+    })
+
+    xhr.addEventListener('error',function(){
+        console.log('error');
+    })
+
+    xhr.open('GET',`https://www.instagram.com/${username}/?__a=1`)
+    xhr.send()
+
+   
+}
